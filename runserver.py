2cea7ad18b1d37b1b21ca910c61fba4b7b6b081f
MSI\Mark <marksmott@gmail.com>
2018-09-26 22:10:43 -0700

    Fixed module call

--- runserver.py
+++ runserver.py
@@ -3,16 +3,9 @@ This script runs the SAIQA application using a development server.
 """
 
 from os import environ
-from SAIQA import app
-
-"""
-
-"""
-import sys, os.path
-sys.path.append('D:/home/site/tools/Lib/site-packages')
+from saiqa import app
 
 if __name__ == '__main__':
-    print('Python Ver:', sys.version)
     HOST = environ.get('SERVER_HOST', 'saiqa.azurewebsites.net')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
--- saiqa/__init__.py
+++ saiqa/__init__.py
@@ -4,10 +4,6 @@ The flask application package.
 
 from flask import Flask
 
-import sys, os.path
-sys.path.append('D:/home/site/tools/Lib/site-packages')
-print('Python Ver:', sys.version)
-
 app = Flask(__name__)
 
-import SAIQA.views
+import saiqa.views
